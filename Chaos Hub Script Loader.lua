78ac4b1139d37483bd77b05beca2dd93b6670a84
Hello(cout) <iwant2play1891@gmail.com>
2025-10-31 18:41:32 +0700

    Chaos Hub Script Loader.lua
    10/31/2025

--- Chaos Hub Script Loader.lua
+++ Chaos Hub Script Loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 537413528 then-- 537413528 Build A Boat For Treasure
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/cccphu12345/Chaos-Hub-Script/refs/heads/main/Build%20a%20boat%20obf.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Chaos-Trash/Chaos-Hub-Script/refs/heads/main/Build%20a%20boat%20obf.lua",true))()
 end
