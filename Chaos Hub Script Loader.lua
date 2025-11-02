a3242949dd5a17668087e75fc46442fc7a083efa
Hello(cout) <iwant2play1891@gmail.com>
2025-11-02 14:46:34 +0700

    Chaos Hub Script Loader.lua

--- Chaos Hub Script Loader.lua
+++ Chaos Hub Script Loader.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 537413528 then-- 537413528 Build A Boat For Treasure
     loadstring(game:HttpGet("https://raw.githubusercontent.com/cccphu12345/Chaos-Hub-Script/refs/heads/main/Build%20a%20boat%20obf.lua",true))()
 elseif game.PlaceId == 15135285305 then-- 15135285305 GEF
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Chaos-Trash/Chaos-Hub-Script/refs/heads/main/GEF%20obf.lua",true))()
+elseif game.GameId == 73754455 then-- 73754455 Game ID
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Chaos-Trash/Chaos-Hub-Script/refs/heads/main/Survive%20and%20Kill%20the%20Killers%20in%20Area%2051",true))()
 end
